98e564836dbb3973e354a6af6bc2101619d643d1
Brian Fish <apparissus@gmail.com>
2020-03-19 13:28:29 -0700

    Add routing (@reach/router)

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
+import { Router, navigate } from '@reach/router';
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Marquee from './Marquee';
+
+function Home() {
+  return (
+    <div class="Home">Welcome home!</div>
+  );
+}
+
+function About() {
+  const logoNavigate = event => {
+    event.preventDefault();
+    navigate("/404");
+  }
+
+  return (
+    <div onClick={logoNavigate} class="About">Not much to say about me, yet.</div>
+  );
+}
+
+const NotFound = () => <h1>Sorry, nothing here.</h1>
 
 function App() {
   return (
@@ -9,6 +29,13 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <Marquee />
+      <div className="App-content">
+        <Router>
+          <Home path="/" />
+          <About path="/about" />
+          <NotFound default />
+        </Router>
+      </div>
       </header>
     </div>
   );
--- src/Marquee.js
+++ src/Marquee.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import { Link } from '@reach/router';
 import './Marquee.css';
 
 function Marquee() {
   return (
     <div className="Marquee">
-        <h1>brian.fish</h1>
+        <Link to="/"><h1>brian.fish</h1></Link>
+        <Link to="/about/"><h2>About</h2></Link>
         <div className="Marquee-split" />
     </div>
   );
