7e030624ede90b3898d5720565005b58ff97bbcc
Brian Fish <apparissus@gmail.com>
2020-03-19 00:04:18 -0700

    Marquee

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Marquee', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/brian.fish/i);
   expect(linkElement).toBeInTheDocument();
 });
---
+++ src/Marquee.js
@@ -0,0 +1,13 @@
+import React from 'react';
+//import './App.css';
+
+function Marquee() {
+  return (
+    <div className="Marquee">
+        <h1>brian.fish</h1>
+    </div>
+  );
+}
+
+export default Marquee;
+
