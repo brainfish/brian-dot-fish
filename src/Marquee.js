a7f8b69e4cbb6731d0890db87e5e6d0cdbae39de
Brian Fish <apparissus@gmail.com>
2020-03-19 00:50:15 -0700

    messing around

--- src/Marquee.js
+++ src/Marquee.js
@@ -1,10 +1,11 @@
 import React from 'react';
-//import './App.css';
+import './Marquee.css';
 
 function Marquee() {
   return (
     <div className="Marquee">
         <h1>brian.fish</h1>
+        <div className="Marquee-split" />
     </div>
   );
 }
