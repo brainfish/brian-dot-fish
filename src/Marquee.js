9ec8b028291e755dc7b0d7983929b3120644bdbf
Brian Fish <apparissus@gmail.com>
2020-03-19 22:27:45 -0700

    new logo

--- src/Marquee.js
+++ src/Marquee.js
@@ -1,15 +1,17 @@
 import cx from 'classnames';
 import React from 'react';
 import { Link } from '@reach/router';
-import logo from './logo.svg';
+import logo from './assets/brian.fish-01-trim.png';
 import './Marquee.scss';
 
 function Marquee({ bigMode }) {
   return (
     <div className={cx('Marquee', { bigMode })}>
       <div className={cx("Marquee-inner", { bigMode })}>
-        <img src={logo} className="Marquee-logo" alt="logo" />
-        <Link to="/"><h1>brian.fish</h1></Link>
+        <Link to="/">
+          <img src={logo} className="Marquee-logo" alt="logo" />
+        </Link>
+        {/* <Link to="/"><h1>brian.fish</h1></Link> */}
       </div>
     </div>
   );
