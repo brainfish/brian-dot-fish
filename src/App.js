3f5fd688f5978edf1893cdbf8c749ef7afa4bfb9
Brian Fish <apparissus@gmail.com>
2020-04-07 12:39:20 -0700

    Add `scope` to manifest, plus misc

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
 import cx from 'classnames';
 import { Router } from '@reach/router';
 import React from 'react';
-import Background from './Background';
 import BackgroundFader from './BackgroundFader';
 import Nav from './Nav';
 import './App.scss';
-// eslint-disable-next-line
-import bg from './assets/video-bg/avrocar-landing-concrete-groun.mp4';
-// eslint-disable-next-line
-import bg2 from './assets/video-bg/avrocar-pilot-testing-avrocar-after-mo.mp4';
 import About from './pages/About';
 import { NotFound } from './pages/Error';
 import Reddit from './pages/Reddit';
