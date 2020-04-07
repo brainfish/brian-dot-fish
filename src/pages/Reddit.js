2d09299082219a208936e11fcc9a09ba8f8ede19
Brian Fish <apparissus@gmail.com>
2020-04-07 16:26:40 -0700

    clean up reddit.js

--- src/pages/Reddit.js
+++ src/pages/Reddit.js
@@ -4,19 +4,19 @@ import './Reddit.css';
 const API_URL = "https://api.brian.fish/reddit";
 
 const RedditPost = ({
-    // author,
+    author,
     title,
-    // ups,
+    upvotes,
     url,
-    // subreddit_name_prefixed,
-    // subreddit_url,
-    // num_comments,
-    // permalink
+    subreddit_name,
+    subreddit_url,
+    num_comments,
+    permalink
 }, key) => {
     return <div className="RedditPost" key={`reddit-post-${key}`}>
-        <a href={url}>{title}</a> by {/*{author}{" "}*/}
-        {/* to <a href={subreddit_url}>{subreddit_name_prefixed}</a> 
-        (<a href={permalink}>{num_comments} comments</a>) ({ups} upvotes) */}
+        <a href={url}>{title}</a> by {author}{" "}
+        to <a href={subreddit_url}>{subreddit_name}</a> 
+        (<a href={permalink}>{num_comments} comments</a>) ({upvotes} upvotes)
     </div>
 }
 
@@ -26,47 +26,22 @@ const RedditPostList = ({ posts }) => {
     </div>
 }
 
-// const testposts = [
-//     {"title": "No corporate bailouts!! Direct financial resources to the working people, not the capitalist elite!", "url": "http://www.wsws.org/en/articles/2020/03/23/pers-m23.html", "author": "exgalactic", "ups": 7132},
-//     {"title": "gqless: GraphQL client without queries", "url": "https://gqless.netlify.com/", "author": "shinework", "ups": 182}
-// ]
-
 class Reddit extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             posts: []
-            // posts: testposts
         };
         this.fetchPosts();
     }
 
-    fetchPosts() {
+    fetchPosts = () => {
         fetch(API_URL)
             .then(response => response.json())
-            .then(data => this.receivePosts(data));
-            // .then(data => {
-            //     console.log(data);
-            //     this.setState({posts: data});
-            // });
-        // console.log('fetching!');
-        // let xhr = new XMLHttpRequest();
-        // xhr.open("GET", API_URL, true);
-
-        // xhr.onload = e => {
-        //     if (xhr.readyState === 4) {
-        //         if (xhr.status === 200) {
-        //             console.log(xhr.responseText);
-        //         } else {
-        //             console.error(xhr.statusText);
-        //         }
-        //     }
-        // };
-        // xhr.onerror = e => console.error(xhr.statusText);
-        // xhr.send(null);
+            .then(this.receivePosts);
     }
 
-    receivePosts(posts) {
+    receivePosts = posts => {
         console.log('got posts: %o', posts);
         this.setState({ posts: posts });
     }
